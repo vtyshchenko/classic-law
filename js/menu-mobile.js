193258b0a2ec7397720fc20625667b87c64ab0fc
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-06-24 00:08:20 +0300

    add menu-mobile.js

---
+++ js/menu-mobile.js
@@ -0,0 +1,9 @@
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
+    menuBtnRef.addEventListener("click", () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  
+  })();
